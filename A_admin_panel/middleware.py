9703adbefc894a664c07cc0749e7aadd358cd453
Ibrokhim1006 <Ibroxim.2001@mail.ru>
2023-02-17 11:20:24 +0500

    Category

--- A_admin_panel/middleware.py
+++ A_admin_panel/middleware.py
@@ -3,7 +3,7 @@ from A_admin_panel.models import *
 
 def all_categoriya(get_res):
     def middleware(request):
-        request.get_catgor = Categoriya.objects.all()
+        request.get_catgor = Categoriya.objects.all().order_by('id')
         response = get_res(request)
 
         return response
